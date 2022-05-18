71319170e946937d43b7a10180f613045dba788b
RonalPerez506 <99605908+RonalPerez506@users.noreply.github.com>
2022-05-17 20:54:04 -0600

    Add files via upload

---
+++ telegrambotpy/bot2.py
@@ -0,0 +1,12 @@
+import schedule
+import telebot
+
+API_TOKEN='5107764356:AAG_OM9tR9tucWPCehs6Wlw5ouYh-znS4eU'
+
+bot = telebot.TeleBot(API_TOKEN)
+@bot.message_handler(commands=["help", "start"])
+
+def enviar(message):
+	bot.reply_to(message, "Hola, soy un bot!")
+
+bot.polling() 
